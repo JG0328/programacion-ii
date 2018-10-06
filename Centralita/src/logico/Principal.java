c9bee9566371ef9c657f92ce142f5ca31dca0658
José González <jgg032899@gmail.com>
2018-10-06 11:36:45 -0400

    Comentarios en la simulación.

--- Centralita/src/logico/Principal.java
+++ Centralita/src/logico/Principal.java
@@ -8,12 +8,16 @@ public class Principal extends Thread {
         c.AddTelefono(t1);
         c.AddTelefono(t2);
         c.start();
+        // Se coge el teléfono
         t1.Descolgar();
         Thread.sleep(2500);
+        // Se marca al teléfono 2 y hablan
         t1.Marcar(2);
         Thread.sleep(6000);
+        // El teléfono 1 cierra
         t1.Colgar(t2);
         Thread.sleep(3000);
+        // El teléfono 2 cierra
         t2.Colgar(null);
     }
 }
